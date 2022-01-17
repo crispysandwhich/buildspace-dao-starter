4d19654c6f538331b0602aafba3f04bb0b42dcc9
crispysandwhich <crispysandwhich@gmail.com>
2022-01-16 21:11:58 -0500

    my little way of handling a wallet connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useMemo, useState } from "react";
+
+// import thirdweb
+import { useWeb3 } from '@3rdweb/hooks';
+
 const App = () => {
+
+  // Use the connectWallet hook thirdweb gives us.
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("Address", address);
+
+  // This is the case where we will handle the page when user hasnt connected their wallet
+  // and are able to use the connectWallet function
+
+  if(!address){
+    return (
+      <div className="landing">
+        <h1>Welcome to NobleFarmerDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">Connect your wallet</button>
+      </div>
+    )
+  }
+
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected ☑</h1>
     </div>
   );
 };
